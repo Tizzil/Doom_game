3668f4ea4f3dc7273d5f424ccde80ab524cac0d7
agthome\Anton <Antontrofimoff@gmail.com>
2017-04-25 18:39:07 +0600

    Немного стрельбы

--- Doom_game/Assets/Scripts/Player.cs
+++ Doom_game/Assets/Scripts/Player.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public GameObject player;
+    public Transform shotSpawn;
+    public GameObject bullet;
     public float speed;
+    public float fireRate = 0.5f;
+    float timer;
 
     private void Update()
     {
         
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        float mouselistener = Input.GetAxis("Fire1");
+        timer += Time.time;
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveHorizontal,moveVertical)*speed;
+        if (Input.GetButton("Fire1") && timer > fireRate)
+        {
+            timer = 0;
+            Instantiate(bullet, shotSpawn.position, shotSpawn.rotation);
+        }
         
 
 
---
+++ Doom_game/Assets/Scripts/Shot_Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot_Mover : MonoBehaviour {
+    float mouseX;
+    float mouseY;
+    public Transform bullet;
+	// Use this for initialization
+	void Start () {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
